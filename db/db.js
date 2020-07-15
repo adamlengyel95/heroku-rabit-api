de27466c15f981be135be12abba665e602524072
9adam <lengyel.adam95@gmail.com>
2020-07-16 01:12:43 +0200

    Db config

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'heroku-demo'
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'b4cf602dd5870f',
+    password: 'c18df09a',
+    database: 'heroku_06eb968c8f57dcb'
 });
 
 module.exports = connection;
