d6dcbc66b33843b99ecda9ab9cea98ccccacb695
9adam <lengyel.adam95@gmail.com>
2020-07-16 00:48:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const db = require('./db/db');
+const port = 5000
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get('/', (req, res) => {
+    db.query(`SELECT * FROM posts`, (err, rows, fields) => {
+        if (err) {
+            const error = { message: 'Error occured during fetching posts', error: err }
+            res.status(500).send(error)
+        } else {
+            res.send(rows)
+        }
+    })
+})
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
